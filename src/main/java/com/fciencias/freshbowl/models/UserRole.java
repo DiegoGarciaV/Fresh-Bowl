6ff9b836fb39f01fe417589bb86efe2728976f04
Diego Garcia <digarcia@icsecurity.com>
2023-11-12 22:20:19 -0600

    Creacion de modelos para entidad User

---
+++ src/main/java/com/fciencias/freshbowl/models/UserRole.java
@@ -0,0 +1,46 @@
+package com.fciencias.freshbowl.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class UserRole {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleId;
+
+    @NotNull(message = "Debe indicarse el nombre del rol")
+    private String roleName;
+    
+    private String roleDescription;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+
+    
+}
